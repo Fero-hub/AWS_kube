1203ea3e503bb73f3be184e25ecd3d2bac683ad2
Fero <frankie.imrich@gmail.com>
2025-10-11 00:58:42 +0200

    3 commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,20 @@
 terraform {
-  required_version = ">= 1.9.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region = eu-central-1
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.1.0.0/16"
 }
 
 # Príklad: S3 bucket
