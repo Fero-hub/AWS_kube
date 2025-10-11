93ab447f4603ceaf9b489c5cb1b1da641a596ffa
Fero <frankie.imrich@gmail.com>
2025-10-11 12:49:23 +0200

    pub IP added

--- terraform/EC2.tf
+++ terraform/EC2.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "web_server" {
   instance_type = "t2.micro"
   key_name      = data.aws_key_pair.ec2_key.key_name
   subnet_id     = aws_subnet.pub_subnet.id
+  associate_public_ip_address = true
 
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
