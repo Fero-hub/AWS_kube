fd405896682ef976e3159da2277527496ec099c7
Fero <frankie.imrich@gmail.com>
2025-10-11 13:41:35 +0200

    state added2

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,22 +6,28 @@ resource "aws_s3_bucket" "interna_appka" {
   }
 }
 
-resource "aws_s3_bucket" "tfstate-s3" {
-  bucket = "tfstate-s3" 
+resource "aws_s3_bucket" "tfstate_s3_mojstate" {  # ZMEŇ NÁZOV (bez pomlčiek)
+  bucket = "tfstate-s3-mojstate" 
   
   tags = {
     Name = "tfstate-s3"
   }
 }
 
-
-
-
 resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.tfstate-s3.id
+  bucket = aws_s3_bucket.tfstate_s3_mojstate.id  # OPRAV REFERENCIU
   
   versioning_configuration {
     status = "Enabled"
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.tfstate_s3_mojstate.id
+  
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
