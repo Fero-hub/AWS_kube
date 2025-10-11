604019e30ad404aa5fad2eb33e9e21bedb9155ac
Fero <frankie.imrich@gmail.com>
2025-10-11 15:21:48 +0200

    s3 policy etc2

--- terraform/EC2.tf
+++ terraform/EC2.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "web_server" {     #3.67.194.199
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   iam_instance_profile = aws_iam_instance_profile.test_profile.name
-  
+
   tags = {
     Name = "web_server"
   }
@@ -22,17 +22,34 @@ output "instance_ip" {
   value = aws_instance.web_server.public_ip
 }
 
+resource "aws_iam_role" "role" {
+  name = "test_role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = aws_iam_role.role.name
+}
+
 resource "aws_iam_policy" "s3_rw" {
   name = "s3_rw_for_app"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
+      Effect   = "Allow"
       Action   = [
         "s3:GetObject",
         "s3:PutObject",
         "s3:ListBucket"
       ]
-      Effect   = "Allow"
       Resource = [
         "arn:aws:s3:::interna-appka",
         "arn:aws:s3:::interna-appka/*"
@@ -45,3 +62,4 @@ resource "aws_iam_role_policy_attachment" "s3_rw_attach" {
   role       = aws_iam_role.role.name
   policy_arn = aws_iam_policy.s3_rw.arn
 }
+
